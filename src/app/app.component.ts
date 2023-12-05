1c0bd07ffd364472c820ef4da94d7028c790d392
Cjr37 <carlosclase37@gmail.com>
2023-12-05 20:26:29 +0100

    Tabla reutilizable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EjerciciosAngular';
+  cabecera: string[] = ["Titulo", "Autor"];
+  datosLibros: any[] = [
+    { "Titulo": "El quijote", "Autor": "Cervantes" },
+    { "Titulo": "100 años de soledad", "Autor": "Gabriel García Márquez" },
+    { "Titulo": "Los pilares de la Tierra", "Autor": "Ken Follet" }
+  ];
+  datosRevistas: any[] = [
+    { "Titulo": "El quijote", "Autor": "Cervantes" },
+    { "Titulo": "100 años de soledad", "Autor": "Gabriel García Márquez" },
+    { "Titulo": "Los pilares de la Tierra", "Autor": "Ken Follet" }
+  ];
+  datosMusica: any[] = [
+    { "Titulo": "El quijote", "Autor": "Cervantes" },
+    { "Titulo": "100 años de soledad", "Autor": "Gabriel García Márquez" },
+    { "Titulo": "Los pilares de la Tierra", "Autor": "Ken Follet" }
+  ];
+  datosCine: any[] = [
+    { "Titulo": "El quijote", "Autor": "Cervantes" },
+    { "Titulo": "100 años de soledad", "Autor": "Gabriel García Márquez" },
+    { "Titulo": "Los pilares de la Tierra", "Autor": "Ken Follet" }
+  ];
 }
